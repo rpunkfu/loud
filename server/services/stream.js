2da9dc78005b6f108f1b8dfa17097f3f7a7ed776
rpunkfu <oskar.cieslik@gmail.com>
2016-11-10 23:22:37 +0100

    add Accept-Ranges header

--- server/services/stream.js
+++ server/services/stream.js
@@ -1,6 +1,8 @@
 import path from 'path'
 
 import Koa from 'koa'
+import cors from 'koa-cors'
+import range from 'koa-range'
 import serve from 'koa-static'
 
 const rootPath = path.join(__dirname, '..', 'uploads', 'songs')
@@ -8,7 +10,10 @@ const rootPath = path.join(__dirname, '..', 'uploads', 'songs')
 export default () => {
   const app = new Koa()
 
-  app.use(serve(rootPath))
+  app
+    .use(range)
+    .use(cors())
+    .use(serve(rootPath))
 
   return app
 }
